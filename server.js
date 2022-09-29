e50fb797c4d9d9eae32bf16df67b50f1b7ac67fb
idris923 <idrisd302@gmail.com>
2022-09-29 13:35:09 +0100

    New commited files

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB=require('./config/db')
+const errorHandler=require('./middleware/error')
+
+//Route files
 
 
 dotenv.config({ path: './config/config.env'});
-
+connectDB()
+const bootcamps=require('./routes/bootcamps')
 const app = express();
+//body parser
+app.use(express.json())
+
+
+// dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+app.use(morgan('dev'));
+}
+
+//Mount routers
+app.use('/api/v1/bootcamps',bootcamps)
+app.use(errorHandler)
+
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server =app.listen(
 PORT, 
 console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+
+process.on('unhandledRejection',(err,promise)=>{
+
+console.log(`Error: ${err.message}`)
+server.close(()=>process.exit(1) )
+})
